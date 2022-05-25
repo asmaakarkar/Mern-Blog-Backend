2c13137ea4b4852f2c0df687960c432f2bc7332d
asmaa <asmaakarkar96@gmail.com>
2022-05-25 21:57:45 +0300

    Add CRUD for users and add Post modal

---
+++ models/Post.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const PostSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    userName: {
+      type: String,
+      required: true,
+    },
+    profilePic: {
+      type: String,
+      required: false,
+    },
+    categories: {
+      type: Array,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Post", PostSchema);
